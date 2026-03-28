e165be9a67baebdf40ffe4804456db79e4403b59
Rameshwaram Varun Teja <tejavarun689@gmail.com>
2026-03-28 11:40:54 +0530

    Added 1public and 2private route tables

--- three-tier-module/modules/infrastructure/main.tf
+++ three-tier-module/modules/infrastructure/main.tf
@@ -142,3 +142,85 @@ resource "aws_nat_gateway" "nat_2" {
 
   depends_on = [aws_internet_gateway.dev]
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.dev.id
+
+  tags = {
+    Name = "dev-public-route-table"
+  }
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.dev.id
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private_1a" {
+  vpc_id = aws_vpc.dev.id
+
+  tags = {
+    Name = "dev-private-route-table-1a"
+  }
+}
+
+resource "aws_route_table" "private_1b" {
+  vpc_id = aws_vpc.dev.id
+
+  tags = {
+    Name = "dev-private-route-table-1b"
+  }
+}
+
+resource "aws_route" "private_1a_nat" {
+  route_table_id         = aws_route_table.private_1a.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_1.id
+}
+
+resource "aws_route" "private_1b_nat" {
+  route_table_id         = aws_route_table.private_1b.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_2.id
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_1a.id
+}
+
+resource "aws_route_table_association" "private_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private_1a.id
+}
+
+resource "aws_route_table_association" "private_3" {
+  subnet_id      = aws_subnet.private_3.id
+  route_table_id = aws_route_table.private_1a.id
+}
+
+resource "aws_route_table_association" "private_4" {
+  subnet_id      = aws_subnet.private_4.id
+  route_table_id = aws_route_table.private_1b.id
+}
+
+resource "aws_route_table_association" "private_5" {
+  subnet_id      = aws_subnet.private_5.id
+  route_table_id = aws_route_table.private_1b.id
+}
+
+resource "aws_route_table_association" "private_6" {
+  subnet_id      = aws_subnet.private_6.id
+  route_table_id = aws_route_table.private_1b.id
+}
--- three-tier-module/modules/infrastructure/outputs.tf
+++ three-tier-module/modules/infrastructure/outputs.tf
@@ -29,3 +29,13 @@ output "nat_gateway_ids" {
   description = "IDs of the NAT Gateways."
   value       = [aws_nat_gateway.nat_1.id, aws_nat_gateway.nat_2.id]
 }
+
+output "public_route_table_id" {
+  description = "ID of the public route table."
+  value       = aws_route_table.public.id
+}
+
+output "private_route_table_ids" {
+  description = "IDs of the private route tables."
+  value       = [aws_route_table.private_1a.id, aws_route_table.private_1b.id]
+}
